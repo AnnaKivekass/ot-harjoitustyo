a0949067a1148c8dc9602443ee7a8304cf0c5199
Anna Kivekäs <anna.kivekas@helsinki.fi>
2026-03-21 17:18:31 +0200

    Complete week2 exercises 1 and 2

---
+++ laskarit/viikko2/maksukortti/maksukortti.py
@@ -0,0 +1,24 @@
+class Maksukortti:
+    def __init__(self, saldo):
+        self.saldo = saldo
+
+    def syo_edullisesti(self):
+        if self.saldo >= 240:
+            self.saldo -= 240
+
+    def syo_maukkaasti(self):
+        if self.saldo >= 400:
+            self.saldo -= 400
+
+    def lataa_rahaa(self, maara):
+        if maara > 0:
+            self.saldo += maara
+            if self.saldo > 15000:
+                self.saldo = 15000
+
+    def saldo_euroina(self):
+        return self.saldo / 100
+
+    def __str__(self):
+        return f"Kortilla on rahaa {self.saldo / 100:.2f} euroa"
+    
