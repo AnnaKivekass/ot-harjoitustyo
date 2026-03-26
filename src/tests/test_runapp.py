abb442dd277f5afd40fdde325ed486ceceb0f4d2
Anna Kivekäs <anna.kivekas@helsinki.fi>
2026-03-26 15:19:32 +0200

    Add unittest for delete_run

--- src/tests/test_runapp.py
+++ src/tests/test_runapp.py
@@ -11,3 +11,10 @@ class TestRunApp(unittest.TestCase):
 
         runs = self.app.list_runs()
         self.assertEqual(len(runs), 1)
+
+    def test_delete_run(self):
+        self.app.add_run(10, 60, "26.3.2026")
+
+        result = self.app.delete_run(0)
+        self.assertTrue(result)
+        self.assertEqual(len(self.app.runs), 0)
